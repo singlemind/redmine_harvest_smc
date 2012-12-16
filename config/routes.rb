74781887040b555c00d8ec160992f6f9de5feaa5
edward <edward@2d6b8309-cc76-4d36-bf69-99218f57494c>
2012-12-16 21:42:16 +0000

    doc and link back
    Signed-off-by: edward sharp <edward.sharp@singlemindconsulting.com>
    
    git-svn-id: https://svn.singlemindconsulting.net/redmine_harvest_smc/trunk@5 2d6b8309-cc76-4d36-bf69-99218f57494c

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
 
-#get 'harvest', :to => 'harvest_entry#index'
-#get 'harvest_user', :to => 'harvest_entry#harvest_user'
+#rails 3.x routes:
+#match 'harvest' => 'harvest_entry#index', :as => :harvest
+#match 'harvest_user' => 'harvest_entry#harvest_user', :as => :harvest_user
 
-match 'harvest' => 'harvest_entry#index', :as => :harvest
-match 'harvest_user' => 'harvest_entry#harvest_user', :as => :harvest_user
+#rails 2.x routes:
+ActionController::Routing::Routes.draw do |map|
+        map.harvest 'harvest', :controller => 'harvest_entry', :action => 'index'
+        map.harvest_user 'harvest_user', :controller => 'harvest_entry', :action => 'harvest_user'
+end
